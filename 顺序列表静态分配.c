ed8a4c1fc8717b0848a594f70cb3c0784e26f998
Cory Beckett <89347557+MasqueradeReveler@users.noreply.github.com>
2021-08-22 23:21:36 +0800

    Update 顺序列表静态分配.c

--- 顺序列表静态分配.c
+++ 顺序列表静态分配.c
@@ -1,24 +1,29 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include<stdio.h>
 #define MaxSize 10
-
-typedef struct {
+typedef struct
+{
 	int data[MaxSize];
 	int length;
-}SqList;
+}SeqList;
 
-void InitList(SqList* L)
+void InitList(SeqList* L)
 {
 	for (int i = 0; i < MaxSize; i++)
 		L->data[i] = 0;
 	L->length = 0;
 }
 
-int main()
+PrintList(SeqList L)
 {
-	SqList L;
-	InitList(&L);
 	for (int i = 0; i < MaxSize; i++)
 		printf("data[%d]=%d\n", i, L.data[i]);
+}
+
+int main()
+{
+	SeqList L = { {1,2,3,4,5},3 };
+	PrintList(L);
+	InitList(&L);
+	PrintList(L);
 	return 0;
 }
