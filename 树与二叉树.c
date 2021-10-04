a8f6415d23422e84bfd85dfbd9885bab20421248
Cory Beckett <89347557+MasqueradeReveler@users.noreply.github.com>
2021-10-04 23:47:09 +0800

    Update 树与二叉树.c

--- 树与二叉树.c
+++ 树与二叉树.c
@@ -55,3 +55,24 @@ void InOrder2(BiTree)  //中序遍历非递归
 		}
 	}
 }
+
+
+void PreOder2(BiTree T)
+{
+	InitStack(S);               //初始化栈S
+	BiTree p = T;               //p为遍历指针
+	while (p || !IsEmpty(S))    //栈不空或指针不空时循环
+	{
+		if (p)                  //指针一路向左
+		{
+			visit(p);
+			Push(S, p);
+			p = p->lchild;      //左孩子不空继续左走
+		}
+		else
+		{
+			Pop(S, p);          //栈顶元素出栈
+			p = p->rchild;      //向右子树走，p赋值为当前节点右孩子
+		}
+	}
+}
